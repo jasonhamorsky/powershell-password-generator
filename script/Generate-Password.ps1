6f150f1babe8fcc9512f555db5033e1df862ffe0
jasonhamorsky <jason.hamorsky1@outlook.com>
2026-02-21 13:52:17 -0500

    Add files via upload

---
+++ script/Generate-Password.ps1
@@ -0,0 +1,11 @@
+﻿# Generate-Password.ps1
+# Simple PowerShell password generator
+
+param(
+    [int]$Length = 12
+)
+
+$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*'
+$password = -join ((1..$Length) | ForEach-Object { $chars[(Get-Random -Minimum 0 -Maximum $chars.Length)] })
+
+Write-Host "Generated password: $password"
